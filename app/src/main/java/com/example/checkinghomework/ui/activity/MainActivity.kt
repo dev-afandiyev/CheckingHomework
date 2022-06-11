7b261c85e98b6de4ca0ac806196c0fda5ecac77f
praise7thesun <gutshellbreath@gmail.com>
2022-06-11 16:14:04 +0300

    added animation background

--- app/src/main/java/com/example/checkinghomework/ui/activity/MainActivity.kt
+++ app/src/main/java/com/example/checkinghomework/ui/activity/MainActivity.kt
@@ -12,6 +12,7 @@ class MainActivity : BaseActivity<ActivityMainBinding>() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding?.root)
+        supportActionBar?.hide()
     }
 
 }
