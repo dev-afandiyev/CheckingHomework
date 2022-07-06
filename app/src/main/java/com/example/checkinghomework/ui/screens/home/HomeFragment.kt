57532c7591bd84980bfdd5eb919e4e1b62d5bb18
praise7thesun <gutshellbreath@gmail.com>
2022-07-07 02:06:36 +0300

    added caching screen

--- app/src/main/java/com/example/checkinghomework/ui/screens/home/HomeFragment.kt
+++ app/src/main/java/com/example/checkinghomework/ui/screens/home/HomeFragment.kt
@@ -2,6 +2,8 @@ package com.example.checkinghomework.ui.screens.home
 
 import android.graphics.drawable.AnimationDrawable
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.view.View
 import com.example.checkinghomework.R
 import com.example.checkinghomework.databinding.FragmentHomeBinding
@@ -17,11 +19,21 @@ class HomeFragment : BaseFragment<FragmentHomeBinding>() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         setupAnimation()
+        Handler(Looper.getMainLooper()).postDelayed({
+            binding.progressBarCyclic.visibility = View.GONE
+            binding.button.visibility = View.VISIBLE
+        }, 3000)
 
         binding.button.setOnClickListener {
             binding.button.setLoading(true)
             navigator.open(R.id.loginFragment)
         }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        binding.progressBarCyclic.visibility = View.VISIBLE
+        binding.button.visibility = View.GONE
     }
 
     private fun setupAnimation() {
